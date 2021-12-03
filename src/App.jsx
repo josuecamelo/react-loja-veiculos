b608ca94d99fa8f3e5bde48c7da912cafbc22271
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-03 15:16:13 -0300

    Concluindo Configuração de Componentes de Notificação, Loading e Alertas

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import { blue } from "@material-ui/core/colors";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import './global.css';
 import Routes from "./Routes";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { Alert, Confirm, Loading, Notify } from "./views/components";
 
 const theme = createTheme({
   palette: {
@@ -29,6 +30,9 @@ function App() {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <Notify />
+        <Loading />
+        <Alert />
         <Routes />
       </ThemeProvider>
     </Provider>
--- src/store/reducers/rootReduces.js
+++ src/store/reducers/rootReduces.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux'
 
 import loadingReducer from './loading.reducer'
+import notifyReducer from './notify.reducer'
+import alertReducer from './alert.reducer'
 
 const rootReduce = combineReducers({
-    loadingReducer
+    loadingReducer,
+    notifyReducer,
+    alertReducer,
 })
 
 export default rootReduce
