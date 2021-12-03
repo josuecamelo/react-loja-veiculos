6518f7a9f0980f1f0de890360885aef72d1c13c5
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-03 11:37:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Routes from './Routes'
+
+
+const theme = createTheme({
+  palette: {
+      primary: {
+          main: blue[500]
+      },
+  },
+  props: {
+      MuiTextField: {
+          variant: 'outlined',
+          fullWidth: true
+      },
+      MuiSelect: {
+          variant: 'outlined',
+          fullWidth: true
+      }
+  }
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  );
+}
+
+export default App;
