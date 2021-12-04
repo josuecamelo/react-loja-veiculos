6ebf2923c85a0bc7f574be91739a6304330a9811
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-04 08:23:01 -0300

    Autenticação - Parte 1

---
+++ src/config/App.js
@@ -0,0 +1,6 @@
+export const rootUrl = 'http://localhost:8000/'
+export const apiUrl = 'http://localhost:8000/api/'
+export const SCROLL = 200
+export const subdomain = "carcrm.app"
+export const ipServer = "86.9528.36"
+export const MP_PUBLIC_KEY = 'TEST-3f04e8d7-08c6-426c-ac13-d4beae8eb836'
