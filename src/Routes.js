dc415ac471e15af5b63dd19a2c7d8a35daae7959
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-04 09:08:15 -0300

    Autenticação com Redirecionamento OK

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,25 @@
-import React, { Suspense } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { CircularProgress } from '@material-ui/core'
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CircularProgress } from "@material-ui/core";
+import Auth from "./views/auth";
 
 const Routes = () => (
-    <Router>
-        <Suspense fallback={<div className="d-flex justify-content-center mt-5 pt-5"> <CircularProgress/> </div>}>
-            <Switch>
-              <Route exact path="/" component={()=>{ return (<h1>Cadastro</h1>)}} />
-              <Route path="/login" component={()=>{ return (<h1>Login</h1>)}} />
-            </Switch>
-        </Suspense>
-    </Router>
-)
+  <Router>
+    <Suspense
+      fallback={
+        <div className="d-flex justify-content-center mt-5 pt-5">
+          {" "}
+          <CircularProgress />{" "}
+        </div>
+      }
+    >
+      <Switch>
+        <Route exact path="/vehicles" component={ () => {return (<h1>Veículos</h1>)}} />
+        <Route exact path="/login" component={Auth} />
+        <Route exact path="/" component={Auth} />
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 export default Routes;
