415baaeeeb542dfad7684c2000521b6cbf679462
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-04 10:43:39 -0300

    Cadastro de Usuários Funcionando

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,15 @@
-import React, { Suspense } from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { CircularProgress } from "@material-ui/core";
-import Auth from "./views/auth";
+
+const Auth = lazy(() => import('./views/auth'));
+const Register = lazy(() => import('./views/register'))
+/*const Vehicles = lazy(() => import('./view/vehicles'))
+const VehicleEdit = lazy(() => import('./view/vehicles/edit'))
+const Pay = lazy(() => import('./view/pay'))
+const Transactions = lazy(() => import('./view/transactions'))
+const TransactionShow = lazy(() => import('./view/transactions/show'))
+const Steps = lazy(() => import('./view/steps'))*/
 
 const Routes = () => (
   <Router>
@@ -15,7 +23,8 @@ const Routes = () => (
     >
       <Switch>
         <Route exact path="/vehicles" component={ () => {return (<h1>Veículos</h1>)}} />
-        <Route exact path="/login" component={Auth} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Auth} />
         <Route exact path="/" component={Auth} />
       </Switch>
     </Suspense>
--- src/store/reducers/rootReduces.js
+++ src/store/reducers/rootReduces.js
@@ -4,12 +4,14 @@ import loadingReducer from './loading.reducer'
 import notifyReducer from './notify.reducer'
 import alertReducer from './alert.reducer'
 import authReducer from './auth.reducer'
+import registerReducer from './register.reducer'
 
 const rootReduce = combineReducers({
     loadingReducer,
     notifyReducer,
     alertReducer,
     authReducer,
+    registerReducer
 })
 
 export default rootReduce
